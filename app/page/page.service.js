e7beb18b0076622a6e9af01060ebd1cb42221974
PhatHong <phat.hong@newoceaninfosys.com>
2018-07-12 16:20:31 +0700

    Add page service

---
+++ app/page/page.service.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var core_1 = require("@angular/core");
+var PageService = /** @class */ (function () {
+    function PageService() {
+        this.selectedBookingInfo = null;
+    }
+    PageService = tslib_1.__decorate([
+        core_1.Injectable()
+    ], PageService);
+    return PageService;
+}());
+exports.default = PageService;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGFnZS5zZXJ2aWNlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsicGFnZS5zZXJ2aWNlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLHNDQUEyQztBQUczQztJQURBO1FBRVcsd0JBQW1CLEdBQUcsSUFBSSxDQUFDO0lBQ3RDLENBQUM7SUFGb0IsV0FBVztRQUQvQixpQkFBVSxFQUFFO09BQ1EsV0FBVyxDQUUvQjtJQUFELGtCQUFDO0NBQUEsQUFGRCxJQUVDO2tCQUZvQixXQUFXIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgSW5qZWN0YWJsZSB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuXG5ASW5qZWN0YWJsZSgpXG5leHBvcnQgZGVmYXVsdCBjbGFzcyBQYWdlU2VydmljZSB7XG4gICAgcHVibGljIHNlbGVjdGVkQm9va2luZ0luZm8gPSBudWxsO1xufSJdfQ==
